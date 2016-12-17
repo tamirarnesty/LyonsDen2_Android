c75e2b5447df1fcf2123ba0583e62ea29d37598d
goadik <goadik@yandex.ru>
2016-12-16 22:26:14 -0500

    User Activity Layout and java class are finished
    Polished up functionality in classes as well as some tweaks with layout
    buttons. Overall works smooth.

--- app/src/main/java/com/wlmac/lyonsden2_android/ContactActivity.java
+++ app/src/main/java/com/wlmac/lyonsden2_android/ContactActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
--- app/src/main/java/com/wlmac/lyonsden2_android/UserActivity.java
+++ app/src/main/java/com/wlmac/lyonsden2_android/UserActivity.java
@@ -1,6 +1,8 @@
 package com.wlmac.lyonsden2_android;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,13 +16,18 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -42,10 +49,13 @@ public class UserActivity extends AppCompatActivity {
     private ListView drawerList;
     /** The drawer toggler used this activity. */
     private ActionBarDrawerToggle drawerToggle;
-    private String displayNameString, emailString, accessLevelString;
+    private TableLayout extraButtonsContainer;
     private EditText displayName;
     private EditText email;
     private EditText accessLevel;
+    private Button toggleButton;
+    private boolean isShowingExtraButtons = true;
+    SharedPreferences sharedPreferences;
 
     private boolean editing = false;
 
@@ -53,23 +63,24 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_activity);
-
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         instantiateComponents();
-
+        extraButtonsContainer = (TableLayout) findViewById(R.id.USContainer);
+        toggleButton = (Button) findViewById(R.id.USToggleButton);
         // Drawer setup
         rootLayout = (DrawerLayout) findViewById(R.id.LDLayout);
         drawerList = (ListView) findViewById(R.id.LDList);
         drawerToggle = HomeActivity.initializeDrawerToggle(this, rootLayout);
         HomeActivity.setupDrawer(this, drawerList, rootLayout, drawerToggle);
+
+        //something entirely different
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        emailString = user.getEmail();
         FirebaseDatabase.getInstance().getReference("users").child("students").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists())
-                    onContentLoad(dataSnapshot.child("name").getValue(String.class), emailString,
+                    onContentLoad(dataSnapshot.child("name").getValue(String.class), user.getEmail(),
                             dataSnapshot.child("accessLevel").getValue(String.class));
                 else Log.d("An error occured", "database directory isn't correct or database does not exist");
             }
@@ -79,20 +90,34 @@ public class UserActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int textHeight = Retrieve.heightForText("Reset Password", this, 24) + 32; // Accounts for padding
+        extraButtonsContainer.animate().translationYBy(textHeight).setDuration(0).start();
+        toggleButton.animate().translationYBy(textHeight).setDuration(0).start();
+    }
+
+    public void toggleButtons (View view) {
+        int textHeight = Retrieve.heightForText("Reset Password", this, 24) + 32; // Accounts for padding
+        textHeight = (isShowingExtraButtons) ? textHeight * -1 : textHeight;
+        extraButtonsContainer.animate().translationYBy(textHeight).setDuration(300).start();
+        toggleButton.animate().translationYBy(textHeight).setDuration(300).start();
+        toggleButton.setBackgroundResource((isShowingExtraButtons) ? R.drawable.arrow_down_48dp : R.drawable.arrow_up_48dp);
+        isShowingExtraButtons = !isShowingExtraButtons;
+    }
+
     private void onContentLoad(String name, String email, String accessLevel) {
         this.displayName.setText(name);
         this.email.setText(email);
         this.accessLevel.setText(accessLevel);
+        sharedPreferences = this.getSharedPreferences(HomeActivity.sharedPreferencesName, Context.MODE_PRIVATE);
     }
 
     private void instantiateComponents () {
         displayName = (EditText) findViewById(R.id.USNameField);
         email = (EditText) findViewById(R.id.USEmailField);
         accessLevel = (EditText) findViewById(R.id.USAccessField);
-    }
-
-    private void checkClubCode (String code) {
-
     }
 
     public void clubLeadershipApplication (View view) {
@@ -116,9 +141,43 @@ public class UserActivity extends AppCompatActivity {
         alertDialog.show(getSupportFragmentManager(), "ClubCodeDialog");
     }
 
-    public void editAccount (View view) {
-        if (view.getId() == R.id.USUpdate) {
+    public void editAccount (String name, final String email) {
+        if (Retrieve.isInternetAvailable(this)) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            DatabaseReference nameDatabaseReference = FirebaseDatabase.getInstance().getReference("users").
+                    child("students").child(user.getUid()).child("name");
+            nameDatabaseReference.setValue(name);
+            //Re-authentication because it is required by firebase in order to change information inside authentication like in this case - email
+            AuthCredential credential = EmailAuthProvider.getCredential(sharedPreferences.getString("email", null),
+                    sharedPreferences.getString("password", null));
+            user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(email, "Re authentication is successful");
+                    } else {
+                        Log.d(email, "Re authentication is not working");
+                        Log.d(email, task.getException().toString());
+                    }
+                }
+            });
+            user.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(email, "User Email address updated.");
+                        } else {
+                            Log.d(email, "It did not work!");
+                            Log.d(email, task.getException().toString());
+                        }
+                    }
+            });
 
+        }
+        else {
+            Toast.makeText(UserActivity.this, "No internet connection available", Toast.LENGTH_SHORT).show();
+        }
+        /*if (view.getId() == R.id.USUpdate) {
             Log.d("User Activity", "Let's pretend that i'm actually updating your info");
         } else if (view.getId() == R.id.USDelete) {
             try {FirebaseAuth.getInstance().getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -140,7 +199,7 @@ public class UserActivity extends AppCompatActivity {
             }
         } else {
             Log.d("User Activity", "Let's pretend that i'm actually resetting your password");
-        }
+        } */
     }
 
     private void enterEditMode () {
@@ -161,13 +220,38 @@ public class UserActivity extends AppCompatActivity {
         email.setClickable(editing);
         email.setInputType((editing) ? InputType.TYPE_CLASS_TEXT : InputType.TYPE_NULL);
 
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        invalidateOptionsMenu();
         // TODO: MAKE KEYBOARD GO AWAY WHEN !editing
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (editing) {
+            //need new icon for a button
+            getMenuInflater().inflate(R.menu.add_menu, menu);
+            return true;
+        }
+        else {
+            getMenuInflater().inflate(R.menu.edit_menu, menu);
+            return true;
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.editAction)
             enterEditMode();
+        else if (item.getItemId() == R.id.addAction){
+            enterEditMode();
+            editAccount(displayName.getText().toString(), email.getText().toString());
+            Toast.makeText(UserActivity.this, "Information Updated", Toast.LENGTH_SHORT).show();
+        }
         if (drawerToggle.onOptionsItemSelected(item))
             return true;
         return super.onOptionsItemSelected(item);
@@ -183,12 +267,6 @@ public class UserActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.edit_menu, menu);
-        return true;
     }
 
     public void UAButtons (View view) {
@@ -236,6 +314,11 @@ public class UserActivity extends AppCompatActivity {
 
     public void becomeClubLeaderButton(View view) {
     }
+
+    private void checkClubCode (String code) {
+
+    }
+
 }
 
 
